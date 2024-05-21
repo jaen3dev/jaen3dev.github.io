aa643e0dbca8e50e71b30d656697a9eb50f9f9b2
Jean <jaen3dev@proton.me>
2024-05-21 00:35:30 -0300

    Added staggering animations for 2nd route, fixed overflow issue (second page only).

--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { motion } from "framer-motion"
+
 import html5 from '../assets/images/svgs/html5.svg'
 import javascript from '../assets/images/svgs/javascript.svg'
 import css3 from '../assets/images/svgs/css3.svg'
@@ -12,42 +13,94 @@ import figma from '../assets/images/svgs/figma.svg'
 import blender from '../assets/images/svgs/blender-3d.svg'
 import unity from '../assets/images/svgs/unity.svg'
 
+const codeSpanVariant = {
+    initial: {
+        opacity: 0,
+        y: '-4rem',
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            delayChildren: 1,
+            staggerChildren: .1
+        }
+    }
+};
+
+const threeDSpanVariant = {
+    initial: {
+        opacity: 0,
+        y: '-4rem',
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            delayChildren: 2.2,
+            staggerChildren: .1
+        }
+    }
+};
+
 export default function Habilidades() {
 
     return (
         <section id="habilidades">
 
             <div className="content">
-                <h2>As habilidades, ferramentas e tecnologias que eu uso<br /> nos meus projetos.</h2>
+                <motion.h2
+                    initial={{ opacity: 0, x: '-100px' }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: .75, ease: 'backInOut', delay: .2 }}>
+                    As habilidades, ferramentas e tecnologias que eu uso<br /> nos meus projetos.
+                </motion.h2>
 
-                <h2>Desenvolvimento Web:</h2>
-                <div className="stack">
-                    <span><img src={html5} />HTML5</span>
-                    <span><img src={css3} />CSS3</span>
-                    <span><img src={javascript} />JAVASCRIPT</span>
-                    <span><img src={sass} />SASS</span>
-                    <span><img src={react} />REACT</span>
-                    <span><img src={git} />GIT</span>
-                    <span><img src={githubWt} />GITHUB</span>
-                    <span><img src={visualStudio} />VSCODE</span>
-                    <span><img src={figma} />FIGMA</span>
-                </div>
-                <h2>Modelagem 3D:</h2>
-                <div className="stack">
-                    <span><img src={blender} />BLENDER</span>
-                    <span><img src={unity} />UNITY</span>
-                </div>
+                <motion.h2
+                    initial={{ opacity: 0, x: '-100px' }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: .75, ease: 'backInOut', delay: .4 }}>
+                    Desenvolvimento Web:
+                </motion.h2>
+
+                <motion.div
+                    className="stack" variants={codeSpanVariant} initial='initial' animate='animate'>
+                    <motion.span variants={codeSpanVariant}><img src={html5} /> HTML5</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={css3} /> CSS3</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={javascript} /> JAVASCRIPT</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={sass} /> SASS</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={react} /> REACT</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={visualStudio} /> VSCODE</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={git} /> GIT</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={githubWt} /> GITHUB</motion.span>
+                    <motion.span variants={codeSpanVariant}><img src={figma} /> FIGMA</motion.span>
+                </motion.div>
+
+                <motion.h2
+                    initial={{ opacity: 0, x: '-100px' }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: .75, ease: 'backInOut', delay: 1.6 }}>
+                    Modelagem 3D:
+                </motion.h2>
+
+                <motion.div
+                    className="stack" variants={threeDSpanVariant} initial='initial' animate='animate'>
+                    <motion.span variants={threeDSpanVariant}><img src={blender} />BLENDER</motion.span>
+                    <motion.span variants={threeDSpanVariant}><img src={unity} />UNITY</motion.span>
+                </motion.div>
             </div>
+
             {location.pathname === '/habilidades' &&
-                <motion.div className="red-skewed-bg" key={location.pathname}
+                <motion.div className="red-skewed-bg"
+                    key={location.pathname}
                     initial={{
                         opacity: 0,
                         y: '100%'
                     }}
                     animate={{
                         opacity: 1,
-                        height: '130vh',
-                        y: '0vh'
+                        height: '100%',
+                        y: '0'
                     }}
                     exit={{
                         opacity: 0,
@@ -56,7 +109,8 @@ export default function Habilidades() {
                     transition={{
                         duration: .75,
                         ease: 'backInOut'
-                    }} />
+                    }}
+                />
             }
         </section>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,17 +27,26 @@ export default function Home() {
                 {location.pathname === '/' &&
                     <>
                         <div className="content">
-                            <div className="intro-and-socials">
+                            <motion.div className="intro-and-socials"
+                                initial={{ opacity: 0, x: '-200px' }}
+                                animate={{ opacity: 1, x: 0 }}
+                                exit={{ opacity: 0, x: '-10rem' }}
+                                transition={{ duration: .75, ease: 'backInOut', delay: .2 }}
+                            >
                                 <p>Jean Carlos,<br />desenvolvedor frontend <br />entusiasta 3D.</p>
                                 <div className="socials">
                                     <a href="https://github.com/jaen3dev" target="_blank"><img src={github} alt="Github logo, it's a squid." /></a>
                                     <a href="mailto:jaen3dev@proton.me"><img src={protonmail} alt="Protonmail logo, it's an open card." /></a>
                                     <a href="https://www.linkedin.com/in/jean-carlos-ca/" target="_blank"><img src={linkedin} alt="Linkedin logo, it's the letters i and n in lowercase" /></a>
                                 </div>
-                            </div>
-                            <div className="threeD">
+                            </motion.div>
+                            <motion.div className="threeD"
+                                initial={{ opacity: 0, x: '200px' }}
+                                animate={{ opacity: 1, x: 0 }}
+                                exit={{ opacity: 0, x: '10rem' }}
+                                transition={{ duration: .75, ease: 'backInOut', delay: .2 }}>
                                 3D
-                            </div>
+                            </motion.div>
                         </div>
                         <motion.div className="red-skewed-bg" key={location.pathname}
                             initial={{
